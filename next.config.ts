225d34c1c374583654e715e1cf95238eaea0cbc0
Magnus Olsen <magnus@highres.no>
2026-04-23 14:18:37 +0200

    Fix GitHub Pages paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/website-smartblender",
+  assetPrefix: "/website-smartblender/",
 };
 
 export default nextConfig;
